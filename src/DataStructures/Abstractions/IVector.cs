4d734bc7bcdce167500f90a61c3ac2072668882a
Ianislav Vasilev <ianislav_vasilev@abv.bg>
2018-07-10 19:38:37 +0300

    Added initial contract of vector. Implementation still missing. (#6)
    * Added initial contract of vector. Implementation still missing.
    
    * Implemented some of the methods and added tests.

---
+++ src/DataStructures/Abstractions/IVector.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DataStructures.Abstractions
+{
+    public interface IVector<T>
+    {
+        int Count { get; }
+
+        int Capacity { get; }
+
+        bool IsEmpty { get; }
+
+        T this[int index] { get; }
+
+        void Push(T item);
+
+        void Insert(T item, int index);
+
+        T Pop();
+
+        void Delete(int index);
+
+        void Remove(Func<T, bool> predicate);
+
+        int Find(Func<T, bool> predicate);
+
+    }
+}
---
+++ tests/DataStructures.UnitTests/VectorTests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using DataStructures.Implementations;
+using NUnit.Framework;
+
+namespace DataStructures.UnitTests
+{
+    [TestFixture]
+    public class VectorTests
+    {
+        private Vector<int> _vector;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _vector = new Vector<int>();
+        }
+
+        [Test]
+        public void IsEmpty_ShouldReturnTrueWhenVectorIsEmptyAndFalseWhenItIsNot()
+        {
+            // Arrange
+            _vector.Push(1);
+
+            // Act
+            // Assert
+            Assert.IsFalse(_vector.IsEmpty);
+            Assert.IsTrue(new Vector<int>().IsEmpty);
+        }
+
+        [Test]
+        public void Indexer_ShouldReturnItemAtGivenIndex()
+        {
+            // Arrange
+            _vector.Push(2);
+
+            // Act
+            var actual = _vector[0];
+
+            // Assert
+            Assert.AreEqual(2, actual);
+        }
+
+        [TestCase(2)]
+        [TestCase(-1)]
+        public void Indexer_WhenIndexIsInvalidOrLowerThanNumberOfItemsInVector_ShouldThrowException(int index)
+        {
+            // Arrange
+            _vector.Push(3);
+
+            // Act
+            // Assert
+            Assert.Throws<IndexOutOfRangeException>(() =>
+            {
+                var _ = _vector[index];
+            });
+        }
+
+        [Test]
+        public void Push_WhenTheIsEnoughCapacityInTheVector_ShouldInsertItemAtTheBackOfTheVectorAndIncrementCount()
+        {
+            // Arrange
+            _vector.Push(3);
+            _vector.Push(5);
+
+            // Act
+            // Assert
+            Assert.AreEqual(5, _vector[1]);
+            Assert.AreEqual(2, _vector.Count);
+        }
+
+        [Test]
+        public void Push_WhenTheIsNotEnoughCapacityInTheVector_ShouldResizeTheVectorAndThenInsertTheItemAtTheBack()
+        {
+            // Arrange
+            const int initialCapacity = 1;
+            var smallSizeVector = new Vector<int>(initialCapacity);
+            Assert.AreEqual(1, smallSizeVector.Capacity);
+
+            // Act
+            smallSizeVector.Push(3);
+            smallSizeVector.Push(4);
+
+            // Assert
+            Assert.AreEqual(2, smallSizeVector.Capacity * 2);
+        }
+    }
+}
