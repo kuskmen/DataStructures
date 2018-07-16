089f17478f7831a5d9afd1c8af69cfc0b766baba
Yanislav Vasilev <ianislav_vasilev@abv.bg>
2018-07-16 23:28:33 +0300

    Initial implementation of Queue.

---
+++ src/DataStructures/Implementations/Queue.cs
@@ -0,0 +1,23 @@
+﻿namespace DataStructures.Implementations
+{
+    using DataStructures.Abstractions;
+
+    public class Queue<T> : IQueue<T>
+    {
+        private readonly LinkedList<T> _items;
+
+        public Queue()
+        {
+            _items = new LinkedList<T>();
+        }
+
+        public bool IsEmpty => _items.IsEmpty;
+
+        public void Enqueue(T item)
+        {
+            _items.AddBack(item);
+        }
+
+        public T Dequeue() => _items.PopFront();
+    }
+}
