823d8f29524bdc23c1d2ef9baf758affcf42a268
Ianislav Vasilev <ianislav_vasilev@abv.bg>
2018-07-17 23:27:58 +0300

    Initial implementation of Queue. (#8)

---
+++ src/DataStructures/Abstractions/IQueue.cs
@@ -0,0 +1,11 @@
+﻿namespace DataStructures.Abstractions
+{
+    public interface IQueue<T>
+    {
+        bool IsEmpty { get; }
+
+        void Enqueue(T item);
+
+        T Dequeue();
+    }
+}
---
+++ tests/DataStructures.UnitTests/QueueTests.cs
@@ -0,0 +1,43 @@
+﻿namespace DataStructures.UnitTests
+{
+    using NUnit.Framework;
+    using DataStructures.Implementations;
+
+    [TestFixture]
+    public class QueueTests
+    {
+        private Queue<int> _queue;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _queue = new Queue<int>();
+        }
+
+        [Test]
+        public void Enqueue_ShouldAddItemToTheQueueAtTheFront()
+        {
+            // Arrange
+            _queue.Enqueue(3);
+
+            // Act
+            // Assert
+            Assert.IsFalse(_queue.IsEmpty);
+        }
+
+        [Test]
+        public void Dequeue_ShouldReturnItemThatWasFirstAddedToTheQueue()
+        {
+            // Arrange
+            _queue.Enqueue(3);
+            _queue.Enqueue(4);
+            _queue.Enqueue(5);
+            
+            // Act
+            var actual = _queue.Dequeue();
+
+            // Assert
+            Assert.AreEqual(3, actual);
+        }
+    }
+}
