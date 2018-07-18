eefaf24ec5281c86354fc66fc98a712d02e09825
Yanislav Vasilev <ianislav_vasilev@abv.bg>
2018-07-18 23:49:14 +0300

    Started implementation of array queue.

--- src/DataStructures/Implementations/Queue.cs
+++ src/DataStructures/Implementations/Queue.cs
@@ -20,4 +20,34 @@
 
         public T Dequeue() => _items.PopFront();
     }
+
+    public class ArrayQueue<T> : IQueue<T>
+    {
+        private readonly T[] _items;
+        private int _capacity;
+        private const int DefaultCapacity = 16;
+
+        public ArrayQueue() : this(DefaultCapacity)
+        {
+        }
+
+        public ArrayQueue(int capacity)
+        {
+            _items = new T[capacity];
+            _capacity = capacity;
+        }
+
+        public bool IsEmpty => _items.Length == 0;
+        public bool IsFull => _items.Length == _capacity - 1;
+
+        public void Enqueue(T item)
+        {
+            _items[_capacity] = item;
+        }
+
+        public T Dequeue()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
 }
